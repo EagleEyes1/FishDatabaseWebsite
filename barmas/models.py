6ebf0845cd372cede4815c20b5350c749f0461c2
fahd <fahderlangga1@gmail.com>
2022-08-10 17:40:39 +0700

    revisiiii

--- barmas/models.py
+++ barmas/models.py
@@ -3,11 +3,11 @@ from datetime import datetime, date
 
 # Create your models here.
 class Bmasuk(models.Model):
-    j_ikan = models.CharField(default="", max_length = 100, blank= False)
-    k_ikan = models.DecimalField(blank=False, max_digits=99, decimal_places=5)
-    ket = models.CharField(default="", max_length = 10, blank= False)
-    m_kend = models.CharField(max_length = 100, blank= False)
-    p_kend = models.CharField(default="" , max_length = 100, blank= False)
+    j_ikan = models.CharField(default="", max_length = 30, blank= False)
+    k_ikan = models.DecimalField(blank=False, max_digits=10, decimal_places=5)
+    ket = models.CharField(default="", max_length = 5, blank= False)
+    m_kend = models.CharField(max_length = 25, blank= False)
+    p_kend = models.CharField(default="" , max_length = 10, blank= False)
     harga = models.IntegerField(default=0, blank=False) 
     t_datang = models.DateField(auto_now_add=False, auto_now=False, blank=False)
     b_gambar = models.TextField(blank=False)
--- jenisikan/models.py
+++ jenisikan/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 
 class JenisIkan(models.Model):   
-    j_ikan = models.CharField(default="", max_length = 100, blank= False)
+    j_ikan = models.CharField(default="", max_length = 30, blank= False)
     g_ikan = models.TextField(blank=False)
     h_kg = models.IntegerField(blank=False)
     h_ton = models.IntegerField(blank=False)
